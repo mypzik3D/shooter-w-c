aaa9dbbe6ba7e26e7bdb26853547c519fd475c3a
miner may <149926497+mypzik3D@users.noreply.github.com>
2024-05-24 17:39:44 +0400

    small update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 	//Shooter game;
     // Optimal for standard monitors:
     //game.create(720, 480, ShooterConsts::PROJECT_NAME, true);
-    //game.create(1920, 1080, ShooterConsts::PROJECT_NAME, true);
+    game.create(1920, 1080, ShooterConsts::PROJECT_NAME, true);
     //game.create(1920, 1080, ShooterConsts::PROJECT_NAME, true, Consts::BACKGROUND_COLOR, sf::Style::Fullscreen);
 
     // Optimal for MacBook Pro 16 display:
